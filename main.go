64434acb4f634eed67e843a71be6530d566354b1
Vivian Lee <wllee@shoalter.com>
2022-05-27 13:26:18 +0800

    detect enemy

--- main.go
+++ main.go
@@ -41,10 +41,94 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, resp)
 }
 
+func getfacingMe(me PlayerState, enemy PlayerState) (result string) {
+	if me.X==enemy.X {
+		if me.Y>enemy.Y {
+			if enemy.Direction=="S" {
+				return "X"
+			}
+		} else {
+			if enemy.Direction=="N" {
+				return "X"
+			}
+		}
+	} else if me.Y==enemy.Y {
+		if me.X>enemy.X {
+			if enemy.Direction=="E" {
+				return "Y"
+			}
+		} else {
+			if enemy.Direction=="W" {
+				return "Y"
+			}
+		}
+	}
+	return "N"
+}
+
+func escape(input ArenaUpdate, me PlayerState, dir string) (res string){
+	var commands = []string{"R", "L"}
+	switch dir {
+	case "X":
+		switch me.Direction {
+		case "N":
+			if me.X==0 {
+				return "R"
+			} else if me.X==input.Arena.Dimensions[0] {
+				return "L"
+			}
+			return commands[rand2.Intn(2)]
+		case "S":
+			if me.X==0 {
+				return "L"
+			} else if me.X==input.Arena.Dimensions[0] {
+				return "R"
+			}
+			return commands[rand2.Intn(2)]
+		}
+		break
+	case "Y":
+		switch me.Direction {
+		case "E":
+			if me.Y==0 {
+				return "R"
+			} else if me.Y==input.Arena.Dimensions[1] {
+				return "L"
+			}
+			return commands[rand2.Intn(2)]
+		case "W":
+			if me.Y==0 {
+				return "L"
+			} else if me.Y==input.Arena.Dimensions[1] {
+				return "R"
+			}
+			return commands[rand2.Intn(2)]
+		}
+		break
+	}
+	return "F"
+}
+
 func play(input ArenaUpdate) (response string) {
 	log.Printf("IN: %#v", input)
 
-	commands := []string{"F", "R", "L", "T"}
-	rand := rand2.Intn(4)
-	return commands[rand]
+	dir := []string{"X", "Y"}
+
+	var me =  input.Arena.State[input.Links.Self.Href]
+	delete(input.Arena.State, input.Links.Self.Href)
+
+	for _, v := range input.Arena.State {
+		if getfacingMe(v, me)!="N" {
+			return "T"
+		} 
+	}
+
+	for _, v := range input.Arena.State { 
+		var enemyFacingMe = getfacingMe(me, v)
+		if enemyFacingMe!="N" {
+			return escape(input, me, enemyFacingMe)
+		} 
+	}
+
+	return escape(input, me, dir[rand2.Intn(2)])
 }
